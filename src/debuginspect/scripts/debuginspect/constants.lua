23f9a5bae476eea637ab2b344095925180510398
gibberish <gbjgms@gmail.com>
2025-08-15 18:30:57 +0700

    Minor constants cleanup
    very very minor tweak

--- src/debuginspect/scripts/debuginspect/constants.lua
+++ src/debuginspect/scripts/debuginspect/constants.lua
@@ -14,8 +14,8 @@ local DIConstants = {
 
 		BUTTON = {
 			BG_NORMAL = {0.4, 0.4, 0.4, 0.5},
-			FG_NORMAL = {1, 1, 1, 1},
 			BG_FOCUSED = {0.8, 0.8, 0.8, 0.5},
+			FG_NORMAL = {1, 1, 1, 1},
 			FG_FOCUSED = {1, 1, 1, 1},
 		},
 
@@ -27,15 +27,21 @@ local DIConstants = {
 		},
 
 		TYPES = {
-			string = {1, 1, 1, 1},
-			number = {0.6, 1, 0.9, 1},
-			table = {1, 0.7, 0.5, 1},
-			boolean = {0.67, 0.8, 0.96, 1},
+			["string"] = {1, 1, 1, 1},
+			["number"] = {0.6, 1, 0.9, 1},
+			["table"] = {1, 0.7, 0.5, 1},
+			["boolean"] = {0.67, 0.8, 0.96, 1},
 			["function"] = {0.8, 0.6, 1, 1},
-			other = {0.8, 0.6, 1, 1},
+			["other"] = {0.8, 0.6, 1, 1},
 		},
-
 	},
 }
+
+-- debug colors
+-- DIConstants.COLORS.PANEL = {0, 0, 0, 1}
+-- DIConstants.COLORS.OVERLAY_NORMAL = {1, 0, 0, 0.4}
+-- DIConstants.COLORS.OVERLAY_HIGHLIGHTED = {0.2, 1, 0.2, 0.4}
+-- DIConstants.COLORS.BUTTON.BG_NORMAL = {0.4, 0.4, 1, 0.5}
+-- DIConstants.COLORS.BUTTON.BG_FOCUSED = {0.8, 0.8, 1, 0.5}
 
 return DIConstants
